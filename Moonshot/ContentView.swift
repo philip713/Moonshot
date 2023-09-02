43e16b583486f23ad334ffebaeadbab17a32a7b9
philip713 <105677806+philip713@users.noreply.github.com>
2023-09-02 12:56:05 -0600

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Philip Janzel Paradeza on 2023-09-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
