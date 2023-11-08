1c90d2b35f6c26a7877eec5864d8764a3b9fcd4d
philip713 <105677806+philip713@users.noreply.github.com>
2023-11-08 00:15:37 -0700

    toolbar item

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -14,50 +14,124 @@ struct ContentView: View {
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
+    @State var showingGrid = true
     var body: some View {
         NavigationView{
-            ScrollView{
-                LazyVGrid(columns: columns){
-                    ForEach(missions){ mission in
-                        NavigationLink{
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack{
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundStyle(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
+            Group{
+                if showingGrid {
+                    missionsGrid(astronauts: astronauts, missions: missions)
+                    .toolbar{
+                        ToolbarItem{
+                            Button("List View"){
+                                showingGrid = false
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
                         }
                     }
+                } else {
+                    missionsList(astronauts: astronauts, missions: missions)
+                        .toolbar{
+                            ToolbarItem{
+                                Button("Grid View"){
+                                    showingGrid = true
+                                }
+                            }
+                        }
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
         }
+        
     }
 }
 
-
+struct missionsGrid: View{
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    var body: some View{
+        ScrollView{
+            LazyVGrid(columns: columns){
+                ForEach(missions){ mission in
+                    NavigationLink{
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            VStack{
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+struct missionsList: View{
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    var body: some View{
+        ScrollView{
+            LazyVStack(){
+                ForEach(missions){ mission in
+                    NavigationLink{
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            VStack{
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
